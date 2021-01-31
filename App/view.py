b1bc86f5d5973bde89ee135cb0fc6cc3503bb4eb
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-01-31 16:15:15 -0500

    Update view

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Book Tags")
     print("0- Salir")
 
 
@@ -58,6 +59,13 @@ def loadTags():
     Carga los Tags
     """
     return controller.loadTags('GoodReads/tags.csv')
+
+
+def loadBooksTags():
+    """
+    Carga los Book Tags
+    """
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
 
 
 """
@@ -76,6 +84,11 @@ while True:
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
     # TO-DO: Modificaciones para completar el laboratorio 1.
+
+    elif int(inputs[0]) == 3:
+        print("Cargando información de los book tags....")
+        books_tags = loadBooksTags()
+        print('Total de book tags cargados: ' + str(lt.size(books_tags)))
 
     else:
         sys.exit(0)
